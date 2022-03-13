600b54020a93b3d82730f442b6d385b52495b6bb
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-14 02:27:13 +0300

    ft: added hover functionality to portfolio section

--- js/app.js
+++ js/app.js
@@ -13,10 +13,133 @@ $(document).ready(function() {
         $("#design").click(function(){
             $("#design").slideUp('1500');
             $("#design-img").slideDown('1500')
-        })
+        });
     });
 
     // Development Section
 
+    $("#development-img").click(function(){
+        $("#development-img").slideDown('1500').hide('1000');
+        $("#development").show('1500');
+    });
 
-})
+    $("#development").click(function(){
+        $("#development").slideUp('1500');
+        $("#development-img").slideDown('1500');
+    });
+});
+
+// Product Management
+
+$(document).ready(function() {
+    $("#product-img").click(function(){
+        $("#product-img").slideDown('1500').hide('1000');
+        $("#product").show('1500');
+    })
+
+    $("#product").click(function(){
+        $("#product").slideUp('1500');
+        $("#product-img").slideDown('1500')
+    });
+});
+
+//Portfolio Section
+
+//Blacklisted
+
+$(document).ready(function () {
+    $('#image1')
+      .mouseover(function () {
+        $('#shadow1').show();
+      })
+      .mouseout(function () {
+        $('#shadow1').hide();
+      });
+  });
+
+  // Ontario Security Manager
+
+  $(document).ready(function () {
+    $('#image2')
+      .mouseover(function () {
+        $('#shadow2').show();
+      })
+      .mouseout(function () {
+        $('#shadow2').hide();
+      });
+  });
+
+  //Pyramids
+
+  $(document).ready(function () {
+    $('#image3')
+      .mouseover(function () {
+        $('#shadow3').show();
+      })
+      .mouseout(function () {
+        $('#shadow3').hide();
+      });
+  });
+
+  // Cyber
+
+  $(document).ready(function () {
+    $('#image4')
+      .mouseover(function () {
+        $('#shadow4').show();
+      })
+      .mouseout(function () {
+        $('#shadow4').hide();
+      });
+  });
+
+  // Elexa
+
+  $(document).ready(function () {
+    $('#image5')
+      .mouseover(function () {
+        $('#shadow5').show();
+      })
+      .mouseout(function () {
+        $('#shadow5').hide();
+      });
+  });
+
+  // Virtual elements
+
+  $(document).ready(function () {
+    $('#image6')
+      .mouseover(function () {
+        $('#shadow6').show();
+      })
+      .mouseout(function () {
+        $('#shadow6').hide();
+      });
+  });
+
+  // Burned
+
+  $(document).ready(function () {
+    $('#image7')
+      .mouseover(function () {
+        $('#shadow7').show();
+      })
+      .mouseout(function () {
+        $('#shadow7').hide();
+      });
+  });
+
+  // Giraffe
+
+  $(document).ready(function () {
+    $('#image8')
+      .mouseover(function () {
+        $('#shadow8').show();
+      })
+      .mouseout(function () {
+        $('#shadow8').hide();
+      });
+  });
+
+
+  
